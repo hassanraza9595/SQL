a3dd23a75da7e2b385bc31337e5783707d529584
hassanraza9595 <sainbeloved@gmail.com>
2022-08-17 17:12:42 +0500

    procedure with argument passing added

--- sql_class_2.sql
+++ sql_class_2.sql
@@ -9,3 +9,23 @@ begin
 end &&      
 
 call bank_data()
+
+DELIMITER &&
+create procedure min_balance()
+begin
+    select * from bank_details where balance in (select min(balance) from bank_details);
+
+end &&      
+
+call min_balance()
+
+/* create a procedure which take an argument */
+
+DELIMITER &&
+create procedure avg_bal_jobrole(IN jobrole varchar(30))
+Begin
+ select avg(balance) from bank_details where job = jobrole;
+End &&   
+
+call avg_bal_jobrole('admin.')
+call avg_bal_jobrole('management')
