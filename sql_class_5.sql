29c06a2dd31509814a6d7f37717755df864979a9
hassanraza9595 <sainbeloved@gmail.com>
2022-09-05 09:39:34 +0500

    integer to string convert function added

--- sql_class_5.sql
+++ sql_class_5.sql
@@ -9,9 +9,57 @@ use sales
 Delimiter $$
 create function add_to_col1(a int)
 returns int
-deterministic
+deterministic  /* always return same result for any kind of input parameter */
 begin
 declare b int;
 set b = a + 10;
 return b;
 end $$
+
+select add_to_col1(20)
+
+
+select quantity, add_to_col1(quantity) as quantity_plus_10 from sales1;
+
+
+/* create another function */
+
+Delimiter $$
+create function final_profits(profit int, discount int)
+returns int
+Deterministic
+begin 
+	declare new_profit int ;
+	set new_profit = profit - discount ;
+	return new_profit ;
+end $$
+
+select profit, discount, final_profits(profit, discount) from sales1;
+
+/* create another user defined function with three parameter */
+
+Delimiter $$
+create function final_profit_3(profit decimal(20,3), discount decimal(20,3), sales decimal(20,3))
+returns int
+Deterministic
+begin 
+	declare new_profit int ;
+	set new_profit = profit - sales * discount ;
+	return new_profit ;
+end $$
+
+
+select profit, discount, sales , final_profit_3(profit, discount, sales) from sales1;
+
+/* create a function which convert integer into string */
+
+Delimiter $$
+create function int_to_str(a int)
+returns varchar(30)
+deterministic
+begin
+declare b varchar(30);
+set b = a;
+return b;
+end $$
+
